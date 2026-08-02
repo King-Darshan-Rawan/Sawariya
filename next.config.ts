2c7618d5487de2b2e570d9bea4f6cb4b2a6e571e
King-Darshan-Rawan <darshanmalviya9826@gmail.com>
2026-08-03 01:12:08 +0530

    Fix GitHub Pages config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Sawariya",
+  assetPrefix: "/Sawariya",
   images: {
     unoptimized: true,
   },
-
-  basePath: isGithubPages ? "/Sawariya" : "",
-  assetPrefix: isGithubPages ? "/Sawariya/" : "",
 };
 
 export default nextConfig;
